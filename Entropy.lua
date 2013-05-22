bb8339d2e2872ce236237a5eed72093563de6ffd
StormFX <stormfx@gmail.com>
2013-05-22 15:50:30 +0000

    - ToC to 50300. - Update version.

--- Entropy.lua
+++ Entropy.lua
@@ -15,7 +15,7 @@ if not MSQ then return end
 -- Silver
 MSQ:AddSkin("Entropy: Silver", {
 	Author = "StormFX",
-	Version = "5.2.@project-revision@",
+	Version = "5.3.@project-revision@",
 	Shape = "Square",
 	Masque_Version = 40300,
 	Backdrop = {
