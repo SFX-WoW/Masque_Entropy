ac40a4a510ae8681d231feecdad2dd53072cf3af
StormFX <stormfx@gmail.com>
2019-04-11 22:13:13 -0500

    - Update skin for Masque changes.

--- Skins.lua
+++ Skins.lua
@@ -6,6 +6,8 @@
 	* File...: Skins.lua
 	* Author.: StormFX
 
+	Entropy Skins
+
 ]]
 
 -- GLOBALS: GetAddOnMetadata, LibStub
@@ -13,20 +15,19 @@
 local MSQ = LibStub and LibStub("Masque", true)
 if not MSQ then return end
 
+local AddOn, Core = ...
+
 ----------------------------------------
 -- Locals
-----------------------------------------
-
-local AddOn, Core = ...
+---
 
 -- Locale
 local L = Core.Locale
 
 -- Skin Info
 local Version = GetAddOnMetadata(AddOn, "Version")
-local Authors = "StormFX"
 local Websites = {
-	"https://github.com/stormfx/masque_entropy",
+	"https://github.com/StormFX/Masque_Entropy",
 	"https://www.wowace.com/projects/masque-entropy",
 	"https://www.curseforge.com/wow/addons/masque-entropy",
 	"https://www.wowinterface.com/downloads/info8873",
@@ -36,96 +37,73 @@ local Websites = {
 local SKIN_DESC = L["A metallic version of Apathy in the color of %s ore."]
 
 ----------------------------------------
--- Entropy - Silver
-----------------------------------------
+-- Silver
+---
 
 MSQ:AddSkin("Entropy - Silver", {
 	Shape = "Square",
+	Masque_Version = 80100,
 	Group = "Entropy",
 	Title = "Silver",
 	Order = 11,
-	Version = Version,
-	Authors = Authors,
-	Websites = Websites,
+
+	-- Info
 	Description = SKIN_DESC:format("Silver"),
-	Masque_Version = 80000,
+	Version = Version,
+	Author = "StormFX",
+	Websites = Websites,
+
+	-- Skin
 	Backdrop = {
-		Width = 32,
-		Height = 32,
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Backdrop]],
+		Color = {0, 0, 0, 0.5},
+		Width = 25,
+		Height = 25,
+		UseColor = true,
 	},
 	Icon = {
-		Width = 28,
-		Height = 28,
+		TexCoords = {0.02, 0.98, 0.02, 0.98},
+		Width = 27,
+		Height = 27,
 	},
-	Flash = {
-		Width = 32,
-		Height = 32,
-		Color = {1, 0, 0, 0.5},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Overlay]],
-	},
-	Pushed = {
-		Width = 32,
-		Height = 32,
+	Shadow = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Shadow]],
 		Color = {0, 0, 0, 0.5},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Overlay]],
+		Width = 32,
+		Height = 32,
 	},
 	Normal = {
-		Width = 32,
-		Height = 32,
 		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
-	},
-	Disabled = {
-		Hide = true,
-	},
-	Checked = {
 		Width = 32,
 		Height = 32,
-		BlendMode = "BLEND",
-		Color = {0, 0.7, 0.9, 0.7},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
 	},
-	Border = {
-		Width = 32,
-		Height = 32,
-		BlendMode = "BLEND",
-		Color = {0, 1, 0, 0.5},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+	--Disabled = {Hide = true,},
+	Pushed = {
+		Color = {0, 0, 0, 0.5},
+		DrawLayer = "BORDER",
+		DrawLevel = 1,
+		Width = 25,
+		Height = 25,
+		UseColor = true,
 	},
-	Gloss = {
-		Width = 32,
-		Height = 32,
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Gloss]],
-	},
-	AutoCastable = {
-		Width = 54,
-		Height = 54,
-		OffsetX = 0.5,
-		OffsetY = -0.5,
-		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
-	},
-	Highlight = {
-		Width = 32,
-		Height = 32,
+	Flash = {
+		Color = {1, 0, 0, 0.4},
 		BlendMode = "ADD",
-		Color = {1, 1, 1, 0.5},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Highlight]],
-	},
-	Name = {
-		Width = 32,
-		Height = 10,
-		OffsetX = 1,
-		OffsetY = 6,
+		DrawLayer = "BORDER",
+		DrawLevel = 0,
+		Width = 25,
+		Height = 25,
+		UseColor = true,
 	},
 	Count = {
 		Width = 32,
 		Height = 10,
 		OffsetX = -4,
-		OffsetY = 7,
+		OffsetY = 6,
 	},
 	HotKey = {
 		Width = 32,
 		Height = 10,
+		OffsetX = -4,
 		OffsetY = -7,
 	},
 	Duration = {
@@ -133,206 +111,358 @@ MSQ:AddSkin("Entropy - Silver", {
 		Height = 10,
 		OffsetY = -2,
 	},
-	Cooldown = {
-		Width = 25,
-		Height = 25,
+	Checked = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+		Color = {0, 0.7, 0.9, 0.7},
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+	},
+	Border = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+		Aura = {
+			Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+			Width = 32,
+			Height = 32,
+		},
+		Debuff = {
+			Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+			Width = 32,
+			Height = 32,
+		},
+		Enchant = {
+			Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+			Color = {0.6, 0.2, 0.9, 1},
+			Width = 32,
+			Height = 32,
+		},
+		Item = {
+			Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+			Width = 32,
+			Height = 32,
+		},
+	},
+	IconBorder = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+		Width = 32,
+		Height = 32,
+	},
+	SlotHighlight = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+		Color = {0, 0.7, 0.9, 0.7},
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+	},
+	Gloss = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Gloss]],
+		Color = {1, 1, 1, 0.5},
+		Width = 32,
+		Height = 32,
+	},
+	IconOverlay = {
+		Width = 30,
+		Height = 30,
+	},
+	NewAction = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Glow]],
+		Color = {1, 1, 0.6, 0.8},
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+	},
+	SpellHighlight = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Glow]],
+		Color = {1, 1, 0.6, 0.8},
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+	},
+	AutoCastable = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Indicator]],
+		Color = {1, 1, 0, 1},
+		Width = 32,
+		Height = 32,
+	},
+	SearchOverlay = {
 		Color = {0, 0, 0, 0.7},
+		Width = 30,
+		Height = 30,
+		Point = "CENTER",
+		UseColor = true,
 	},
-	ChargeCooldown = {
-		Width = 25,
-		Height = 25,
+	ContextOverlay = {
+		Color = {0, 0, 0, 0.7},
+		Width = 30,
+		Height = 30,
+		Point = "CENTER",
+		UseColor = true,
 	},
-	Shine = {
+	NewItem = {
+		DrawLayer = "BORDER",
+		DrawLevel = 0,
+		Width = 28,
+		Height = 28,
+	},
+	Highlight = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Border]],
+		Color = {1, 1, 1, 0.5},
+		BlendMode = "ADD",
+		Width = 32,
+		Height = 32,
+	},
+	Name = {
+		Width = 32,
+		Height = 10,
+		OffsetY = 5,
+	},
+	AutoCastShine = {
 		Width = 26,
 		Height = 26,
 		OffsetX = 1,
 		OffsetY = -1,
 	},
+	Cooldown = {
+		Color = {0, 0, 0, 0.7},
+		Width = 25,
+		Height = 25,
+	},
+	ChargeCooldown = {
+		Width = 25,
+		Height = 25,
+	},
 }, true)
 
 ----------------------------------------
--- Entropy - Adamantite
-----------------------------------------
+-- Adamantite
+---
 
 MSQ:AddSkin("Entropy - Adamantite", {
+	Template = "Entropy - Silver",
 	Title = "Adamantite",
 	Order = 1,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Adamantite"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.7, 0.8, 0.9, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.7, 0.8, 0.9, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Bronze
-----------------------------------------
+-- Bronze
+---
 
 MSQ:AddSkin("Entropy - Bronze", {
+	Template = "Entropy - Silver",
 	Title = "Bronze",
 	Order = 2,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Bronze"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {1, 0.8, 0, 1},
 		Width = 32,
 		Height = 32,
-		Color = {1, 0.8, 0, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Cobalt
-----------------------------------------
+-- Cobalt
+---
 
 MSQ:AddSkin("Entropy - Cobalt", {
+	Template = "Entropy - Silver",
 	Title = "Cobalt",
 	Order = 3,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Cobalt"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.3, 0.7, 0.9, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.3, 0.7, 0.9, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Copper
-----------------------------------------
+-- Copper
+---
 
 MSQ:AddSkin("Entropy - Copper", {
+	Template = "Entropy - Silver",
 	Title = "Copper",
 	Order = 4,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Copper"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.8, 0.5, 0, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.8, 0.5, 0, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Fel Iron
-----------------------------------------
+-- Fel Iron
+---
 
 MSQ:AddSkin("Entropy - Fel Iron", {
+	Template = "Entropy - Silver",
 	Title = "Fel Iron",
 	Order = 5,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Fel Iron"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.8, 1, 0.8, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.8, 1, 0.8, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Gold
-----------------------------------------
+-- Gold
+---
 
 MSQ:AddSkin("Entropy - Gold", {
+	Template = "Entropy - Silver",
 	Title = "Gold",
 	Order = 6,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Gold"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.8, 0.8, 0, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.8, 0.8, 0, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Iron
-----------------------------------------
+-- Iron
+---
 
 MSQ:AddSkin("Entropy - Iron", {
+	Template = "Entropy - Silver",
 	Title = "Iron",
 	Order = 7,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Iron"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.5, 0.5, 0.5, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.5, 0.5, 0.5, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Khorium
-----------------------------------------
+-- Khorium
+---
 
 MSQ:AddSkin("Entropy - Khorium", {
+	Template = "Entropy - Silver",
 	Title = "Khorium",
 	Order = 8,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Khorium"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {1, 0.8, 0.9, 1},
 		Width = 32,
 		Height = 32,
-		Color = {1, 0.8, 0.9, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Obsidium
-----------------------------------------
+-- Obsidium
+---
 
 MSQ:AddSkin("Entropy - Obsidium", {
+	Template = "Entropy - Silver",
 	Title = "Obsidium",
 	Order = 9,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Obsidium"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.3, 0.3, 0.3, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.3, 0.3, 0.3, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Saronite
-----------------------------------------
+-- Saronite
+---
 
 MSQ:AddSkin("Entropy - Saronite", {
+	Template = "Entropy - Silver",
 	Title = "Saronite",
 	Order = 10,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Saronite"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {0.3, 0.9, 0.7, 1},
 		Width = 32,
 		Height = 32,
-		Color = {0.3, 0.9, 0.7, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
 
 ----------------------------------------
--- Entropy - Titanium
-----------------------------------------
+-- Titanium
+---
 
 MSQ:AddSkin("Entropy - Titanium", {
+	Template = "Entropy - Silver",
 	Title = "Titanium",
 	Order = 12,
-	Template = "Entropy - Silver",
+
+	-- Info
 	Description = SKIN_DESC:format("Titanium"),
+
+	-- Skin
 	Normal = {
+		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
+		Color = {1, 1, 0.7, 1},
 		Width = 32,
 		Height = 32,
-		Color = {1, 1, 0.7, 1},
-		Texture = [[Interface\AddOns\Masque_Entropy\Textures\Normal]],
 	},
 }, true)
